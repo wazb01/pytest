97a706beb998691bc57d7d9c2f158b2a16158244
Daniel <daniel@datascientest.com>
2025-02-10 02:02:56 +0000

    modif

--- file_test.py
+++ file_test.py
@@ -5,17 +5,20 @@ def test_calc_addition():
     output = 2+4
     assert output == 6
 
+
 def test_calc_substraction():
     # Fonction test du résultat de 2-4
     output = 2-4
     assert output == -2
+
 
 def test_calc_multiply():
     # Fonction test du résultat de 2*4
     output = 2*4
     assert output == 8
 
+
 def test_coucou():
     # Fonction test si la résultat renvoie 'hello'
-    output='hello'
+    output = 'hello'
     assert output == 'hello'
